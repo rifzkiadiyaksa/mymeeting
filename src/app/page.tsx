0db42f6e7d9bd5acae9b871c281191809889859c
RIFZKI ADIYAKSA <rifzkiadiyaksa.mh@gmail.com>
2026-04-08 16:27:40 +0700

    fix bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { supabase } from '@/lib/supabase/client'
+import { supabase } from '../lib/supabase/client'
 
 export default async function Home() {
   const { data, error } = await supabase.auth.getSession()
